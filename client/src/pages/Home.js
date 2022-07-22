d5a691498f892b9d8e7cffac2ab7306f30c0a064
christine chang <christine_chang@yahoo.com>
2022-07-22 21:11:25 +0200

    update notes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,19 +12,18 @@ const Home = (props) => {
         <li>Handle bad or no URL </li>
         <li>Handle Date - correct format but, doesn't check actual valid date - ie: 2/30/96 passes</li>
         <li>Handle input arrays for categories, authors</li>
+        <li>Create id for each book - combine ISBN and part title (some books missing ISBN)</li>
       </ul>
       <p>Still To do: </p>
       <ul>
+        <li>Typescript</li>
+        <li>Testing</li>
         <li>Post changes </li>
         <li>Edit existing book</li>
         <li>Read/Write from file</li>
-        <li>Typescript</li>
-        <li>Testing</li>
+
       </ul>
-      <p> Note that initially ISBN was used for ID, but the database had several
-        books without ISBN, so I create an ID on entry which is used to index into
-        book array to get info to display full.
-      </p>
+
       <h3>note - to get this running:</h3>
       <p> (open terminal in vsCode in ameelioChallenge dir) cd server; npm start</p>
       <p> (open another terminal) cd client; npm start</p>
