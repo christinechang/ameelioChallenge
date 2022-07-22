e924b1a2ef21ca50025ad0cc104ea51a37d3e75d
christine chang <christine_chang@yahoo.com>
2022-07-22 21:09:22 +0200

    add notes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,14 +11,15 @@ const Home = (props) => {
         <li>Some input items are required</li>
         <li>Handle bad or no URL </li>
         <li>Handle Date - correct format but, doesn't check actual valid date - ie: 2/30/96 passes</li>
+        <li>Handle input arrays for categories, authors</li>
       </ul>
       <p>Still To do: </p>
       <ul>
         <li>Post changes </li>
         <li>Edit existing book</li>
         <li>Read/Write from file</li>
-        <li>Handle input arrays for categories, authors</li>
         <li>Typescript</li>
+        <li>Testing</li>
       </ul>
       <p> Note that initially ISBN was used for ID, but the database had several
         books without ISBN, so I create an ID on entry which is used to index into
